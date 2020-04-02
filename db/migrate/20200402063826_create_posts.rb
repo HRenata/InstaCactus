0bb62d45bc9fd0958061e1d944595e6331ef7d11
Renata <chromic507@gmail.com>
2020-04-02 09:50:57 +0300

    LAB_3 creating posts db

--- db/migrate/20200402063826_create_posts.rb
+++ db/migrate/20200402063826_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
+    drop_table :posts
     create_table :posts do |t|
       t.string :description
+      t.integer :user_id
 
       t.timestamps
     end
