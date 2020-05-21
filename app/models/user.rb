8bd7129d39fd872a66d8f15311551b0d343766b6
Renata <chromic507@gmail.com>
2020-05-21 15:47:53 +0300

    LAB_4 added following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable,
         :lockable, :confirmable
   has_many :posts, dependent: :destroy
+
+  has_many :active_following, class_name: 'Following', foreign_key: 'follower_id',
+  dependent: :destroy
+  has_many :passive_following, class_name: 'Following', foreign_key: 'followed_id',
+  dependent: :destroy
+  has_many :following, through: :active_following,  source: :followed
+  has_many :followers, through: :passive_following, source: :follower
+
+  def follow(user)
+    active_following.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_following.find_by(followed_id: user.id).destroy
+  end  
+
+  def following?(user)
+    following.include?(user)
+  end  
+
 end
